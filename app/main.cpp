bcc5e9e45aba4ac7ff1d1515db18e9b6269dc91c
amrish1222 <amrish1222@gmail.com>
2018-11-30 23:04:25 -0500

    Modified for running the controller class

--- app/main.cpp
+++ app/main.cpp
@@ -5,7 +5,14 @@
  * Copyright 2018 Amrish Baskaran
  */
 #include <iostream>
+#include "../include/Controller.h"
 
 int main() {
+  PID pid;
+  Controller _controller(pid);
+  if (_controller.runController(10) == 0) {
+    std::cout << "Target Velocity= 10 Reached!!" << std::endl;
+  }
+
   return 0;
 }
